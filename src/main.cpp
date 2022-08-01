31fd277ba47712faf47956b15a3e37b4a6dc5e35
Joel <emmanuel-joseph.ellaga@hotmail.com>
2022-08-01 12:58:14 +0800

    Update main.cpp
    Created checking script

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.print("setup() function running on core: ");
+  Serial.println(xPortGetCoreID());
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.print("loop() function running on core: ");
+  Serial.println(xPortGetCoreID());
 }
